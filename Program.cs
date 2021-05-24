7806bd4a2aa9d541a1c9914fe19feeddd330e565
Pedro Ricardo <pedrochavez645@live.com>
2021-05-24 14:27:38 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Instruccion ForEach";
+            int[] Edades = new int[5];
+            Array(Edades);
+            Console.ReadKey();
+
+        }
+
+        static void Array(int[] Edades)
+        {
+            int Total = 0;
+            Edades = new int[5];
+            for(int x = 0; x < Edades.Length; x++)
+            {
+                Console.WriteLine("Ingresa la edad del estudiante " + (x + 1) + ":");
+                Edades[x] = int.Parse(Console.ReadLine());
+            }
+            foreach(int element in Edades)
+            {
+                Total = Total + element;
+            }
+
+            Console.WriteLine("La sumatoria de las edades es: " + Total);
+
+        }
+    }
+}
